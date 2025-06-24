09c1a4771279553355711c1ce0c65014255c8b78
Praveen Kumar Bonakurthi <prbona@microsoft.com>
2025-06-24 16:50:51 -0700

    Initial project setup

---
+++ src/EL-Agent-Api/Bot/Agents/OfferingsAgent.cs
@@ -0,0 +1,41 @@
+﻿using Azure.AI.Agents.Persistent;
+using Azure.Identity;
+using Microsoft.SemanticKernel;
+using Microsoft.SemanticKernel.Agents;
+using Microsoft.SemanticKernel.Agents.AzureAI;
+using Microsoft.SemanticKernel.Agents.Chat;
+using Microsoft.SemanticKernel.ChatCompletion;
+using ElAgentApi.Bot.Plugins;
+using System.Configuration;
+using System.Text;
+
+
+namespace ElAgentApi.Bot.Agents
+{
+    public class OfferingsAgent
+    {
+#pragma warning disable SKEXP0110 // Type is for evaluation purposes only and is subject to change or removal in future updates. Suppress this diagnostic to proceed.
+        AzureAIAgent aiAgent;
+
+        private PersistentAgentsClient agentsClient;
+
+        public OfferingsAgent(IConfiguration configuration)
+        {
+            agentsClient = AzureAIAgent.CreateAgentsClient(configuration.GetValue<string>("AIProjectConnectionString")!, new DefaultAzureCredential());
+            var agent = agentsClient.Administration.GetAgent("asst_YK2flcJLkjtQBgnEC9qkJsiN");
+            aiAgent = new(agent, agentsClient);
+        }
+
+        public async IAsyncEnumerable<ChatMessageContent> InvokeAgentAsync(string input, ChatHistory chatHistory)
+        {
+            AzureAIAgentThread agentThread = new(aiAgent.Client);
+            
+            ChatMessageContent message = new(AuthorRole.User, input);
+            await foreach (ChatMessageContent response in aiAgent.InvokeAsync(message, agentThread))
+            {
+                chatHistory.Add(response);
+                yield return response;
+            }
+        }
+    }
+}
---
+++ src/EL-Agent-Api/Bot/OfferingAgentBot.cs
@@ -0,0 +1,116 @@
+﻿using Azure;
+using Microsoft.Agents.Builder;
+using Microsoft.Agents.Builder.App;
+using Microsoft.Agents.Builder.State;
+using Microsoft.Agents.Core.Models;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using Microsoft.SemanticKernel;
+using Microsoft.SemanticKernel.ChatCompletion;
+using ElAgentApi.Bot.Agents;
+
+namespace ElAgentApi.Bot
+{
+    public class OfferingAgentBot : AgentApplication
+    {
+        OfferingsAgent _offeringsAgent;
+        private Kernel _kernel;
+        private readonly IConfiguration configuration;
+
+        public OfferingAgentBot(AgentApplicationOptions options, Kernel kernel, IConfiguration configuration, OfferingsAgent offeringsAgent) : base(options)
+        {
+            _kernel = kernel ?? throw new ArgumentNullException(nameof(kernel));
+            this.configuration = configuration;
+            _offeringsAgent = offeringsAgent;
+            OnConversationUpdate(ConversationUpdateEvents.MembersAdded, WelcomeMessageAsync);
+            OnActivity(ActivityTypes.Message, MessageActivityAsync, rank: RouteRank.Last);
+        }
+
+        private async Task MessageActivityAsync(ITurnContext turnContext, ITurnState turnState, CancellationToken cancellationToken)
+        {
+            // Setup local service connection
+            ServiceCollection serviceCollection = [
+                new ServiceDescriptor(typeof(ITurnState), turnState),
+                new ServiceDescriptor(typeof(ITurnContext), turnContext),
+                new ServiceDescriptor(typeof(Kernel), _kernel),
+            ];
+
+            // Start a Streaming Process 
+            await turnContext.StreamingResponse.QueueInformativeUpdateAsync("Working on a response for you");
+
+            ChatHistory chatHistory = turnState.GetValue("conversation.chatHistory", () => new ChatHistory());
+
+            // Invoke the WeatherForecastAgent to process the message
+            await foreach (ChatMessageContent response in _offeringsAgent.InvokeAgentAsync(turnContext.Activity.Text, chatHistory))
+            {
+                if (response == null)
+                {
+                    turnContext.StreamingResponse.QueueTextChunk("Sorry, I couldn't get the offerings at the moment.");
+                    await turnContext.StreamingResponse.EndStreamAsync(cancellationToken);
+                    return;
+                }
+
+                chatHistory.Add(response);
+                turnContext.StreamingResponse.QueueTextChunk(response.Content);
+            }
+
+            ////turnContext.StreamingResponse.QueueTextChunk(GetVideoAdaptiveCard());
+            //turnContext.StreamingResponse.FinalMessage = MessageFactory.Attachment(new Attachment()
+            //{
+            //    ContentType = "application/vnd.microsoft.card.adaptive",
+            //    Content = GetVideoAdaptiveCard(),
+            //});
+
+            await turnContext.StreamingResponse.EndStreamAsync(cancellationToken); // End the streaming response
+        }
+
+        private async Task WelcomeMessageAsync(ITurnContext turnContext, ITurnState turnState, CancellationToken cancellationToken)
+        {
+            foreach (ChannelAccount member in turnContext.Activity.MembersAdded)
+            {
+                if (member.Id != turnContext.Activity.Recipient.Id)
+                {
+                    await turnContext.SendActivityAsync(MessageFactory.Text("Hello and Welcome! I'm here to help with all your offerings and program needs!"), cancellationToken);
+                }
+            }
+        }
+
+        private string GetVideoAdaptiveCard()
+        {
+            // This method can be used to return an adaptive card for video content.
+            // For now, it returns a placeholder string.
+            return """
+            {
+              "type": "AdaptiveCard",
+              "version": "1.5",
+              "body": [
+                {
+                  "type": "Media",
+                   "altText": "This is a test video",
+                    "poster": "https://adaptivecards.microsoft.com/images/adaptivecards1.jpeg",
+                    "id": "media",
+                    "sources": [
+                        {
+                            "mimeType": "video/mp4",
+                            "url": "https://employeehub-my.sharepoint.com/:v:/g/personal/prbona_employeehub_onmicrosoft_com/ETG4mZkmvspKg2ap8YcAl10BnTC9Gc7zQBx0CD76GGj0Ig?e=21bggE&nav=eyJyZWZlcnJhbEluZm8iOnsicmVmZXJyYWxBcHAiOiJTdHJlYW1XZWJBcHAiLCJyZWZlcnJhbFZpZXciOiJTaGFyZURpYWxvZy1MaW5rIiwicmVmZXJyYWxBcHBQbGF0Zm9ybSI6IldlYiIsInJlZmVycmFsTW9kZSI6InZpZXcifX0%3D"
+                        }
+                    ]
+                },
+                {
+                  "type": "TextBlock",
+                  "text": "Telegraph Road - Dire Straits",
+                  "size": "Large",
+                  "weight": "Bolder",
+                  "wrap": true
+                },
+                {
+                  "type": "TextBlock",
+                  "spacing": "None",
+                  "text": "Cover by David Claux",
+                  "wrap": true
+                }
+              ]
+            }
+            """;
+        }
+    }
+}
