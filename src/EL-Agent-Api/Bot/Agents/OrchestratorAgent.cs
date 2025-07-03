95eb20c97e8d399dbda41b4f1a53dd0bfdf5828a
Praveen Kumar Bonakurthi <prbona@microsoft.com>
2025-07-02 22:17:52 -0700

    Refactoring code

--- src/EL-Agent-Api/Bot/Agents/OrchestratorAgent.cs
+++ src/EL-Agent-Api/Bot/Agents/OrchestratorAgent.cs
@@ -13,9 +13,9 @@ namespace ElAgentApi.Bot.Agents;
 
 public class OrchestratorAgent
 {
-    private readonly Kernel _kernel;
+    private readonly Kernel kernel;
     private readonly IServiceProvider service;
-    private readonly ChatCompletionAgent _agent;
+    private readonly ChatCompletionAgent agent;
 
     /// <summary>
     /// Initializes a new instance of the <see cref="OrchestratorAgent"/> class.
@@ -23,28 +23,27 @@ public class OrchestratorAgent
     /// <param name="kernel">An instance of <see cref="Kernel"/> for interacting with an LLM.</param>
     public OrchestratorAgent(Kernel kernel, IServiceProvider service)
     {
-        _kernel = kernel;
+        this.kernel = kernel;
         this.service = service;
 
         // Define the agent
-        _agent =
+        agent =
             new(ReadFileForPromptTemplateConfig("./Bot/Agents/Prompts/Agent.yaml"), templateFactory: new KernelPromptTemplateFactory())
             {
-                Kernel = _kernel,
+                Kernel = this.kernel,
                 Arguments = new KernelArguments(new OpenAIPromptExecutionSettings() 
                 { 
                     FunctionChoiceBehavior = FunctionChoiceBehavior.Auto(), 
                     //ResponseFormat = "json_object",
-                    //ToolCallBehavior = ToolCallBehavior.AutoInvokeKernelFunctions,
                     Temperature = 0.1,
                     MaxTokens = 500,
                 }),
             };
 
         // Give the agent some tools to work with
-        _agent.Kernel.Plugins.Add(KernelPluginFactory.CreateFromType<DateTimePlugin>(serviceProvider: service));
-        _agent.Kernel.Plugins.Add(KernelPluginFactory.CreateFromType<OfferingsPlugin>(serviceProvider: service));
-        _agent.Kernel.Plugins.Add(KernelPluginFactory.CreateFromType<MathPlugin>(serviceProvider: service));
+        agent.Kernel.Plugins.Add(KernelPluginFactory.CreateFromType<DateTimePlugin>(serviceProvider: service));
+        agent.Kernel.Plugins.Add(KernelPluginFactory.CreateFromType<OfferingsPlugin>(serviceProvider: service));
+        agent.Kernel.Plugins.Add(KernelPluginFactory.CreateFromType<MathPlugin>(serviceProvider: service));
     }
 
     /// <summary>
@@ -52,17 +51,21 @@ public class OrchestratorAgent
     /// </summary>
     /// <param name="input">A message to process.</param>
     /// <returns>An instance of <see cref="WeatherForecastAgentResponse"/></returns>
-    public async Task InvokeAgentAsync(string input, ChatHistory chatHistory, CancellationToken cancellationToken = default)
+    public async Task InvokeAgentAsync(ChatMessageContent message, ChatHistory chatHistory, CancellationToken cancellationToken = default)
     {
         ArgumentNullException.ThrowIfNull(chatHistory);
         AgentThread thread = new ChatHistoryAgentThread();
-        ChatMessageContent message = new(AuthorRole.User, input);
         chatHistory.Add(message);
         var turnContext = service.GetService<ITurnContext>();
 
-        await foreach (var response in this._agent.InvokeStreamingAsync(chatHistory, thread: thread, cancellationToken: cancellationToken))
+        await foreach (var response in this.agent.InvokeStreamingAsync(chatHistory, thread: thread, cancellationToken: cancellationToken))
         {
-            //chatHistory.Add(response.Message);
+            if (response.Message.Content is null)
+            {
+                continue;
+            }
+
+            chatHistory.Add(new ChatMessageContent(AuthorRole.Assistant, response.Message.Content));
             turnContext!.StreamingResponse.QueueTextChunk(response.Message.Content!);
         }
 
--- src/EL-Agent-Api/Bot/OfferingAgentBot.cs
+++ src/EL-Agent-Api/Bot/OfferingAgentBot.cs
@@ -13,17 +13,13 @@ namespace ElAgentApi.Bot
 {
     public class OfferingAgentBot : AgentApplication
     {
-        OrchestratorAgent orchestratorAgent;
-        private Kernel _kernel;
+        private Kernel kernel;
         private readonly IConfiguration configuration;
-        private readonly ChatCompletionAgent _chatCompletionAgent;
 
         public OfferingAgentBot(AgentApplicationOptions options, Kernel kernel, IConfiguration configuration) : base(options)
         {
-            _kernel = kernel ?? throw new ArgumentNullException(nameof(kernel));
+            this.kernel = kernel ?? throw new ArgumentNullException(nameof(kernel));
             this.configuration = configuration;
-            //_chatCompletionAgent = chatCompletionAgent;
-            //_offeringsAgent = offeringsAgent;
             OnConversationUpdate(ConversationUpdateEvents.MembersAdded, WelcomeMessageAsync);
             OnActivity(ActivityTypes.Message, MessageActivityAsync, rank: RouteRank.Last);
         }
@@ -34,7 +30,7 @@ namespace ElAgentApi.Bot
             ServiceCollection serviceCollection = [
                 new ServiceDescriptor(typeof(ITurnState), turnState),
                 new ServiceDescriptor(typeof(ITurnContext), turnContext),
-                new ServiceDescriptor(typeof(Kernel), _kernel),
+                new ServiceDescriptor(typeof(Kernel), kernel),
                 new ServiceDescriptor(typeof(OfferingsAgent), sp => new OfferingsAgent(this.configuration, turnContext), ServiceLifetime.Singleton),
             ];
 
@@ -43,7 +39,7 @@ namespace ElAgentApi.Bot
             // Start a Streaming Process 
             await turnContext.StreamingResponse.QueueInformativeUpdateAsync("Working on a response for you");
 
-            orchestratorAgent = new OrchestratorAgent(_kernel, serviceCollection.BuildServiceProvider());
+            var orchestratorAgent = new OrchestratorAgent(kernel, serviceCollection.BuildServiceProvider());
 
             // create the chat message to send to the agent
             var message = new ChatMessageContent(AuthorRole.User, turnContext.Activity.Text);
@@ -52,39 +48,7 @@ namespace ElAgentApi.Bot
             await turnContext.StreamingResponse.QueueInformativeUpdateAsync("Working on a response for you", cancellationToken);
 
             ChatHistory chatHistory = turnState.GetValue("conversation.chatHistory", () => new ChatHistory());
-            //var agentThread = new ChatHistoryAgentThread();
-            //await foreach(var delta in orchestratorAgent.InvokeAgentAsync(turnContext.Activity.Text, agentThread, cancellationToken)
-            //{
-            //    // Process each chunk of the response
-            //    // Here you can handle the streaming response as needed
-            //    // For example, you can queue the text chunk to the streaming response
-            //    turnContext.StreamingResponse.QueueTextChunk(delta.Message.Content!);
-            //}
-            await orchestratorAgent.InvokeAgentAsync(turnContext.Activity.Text, chatHistory, cancellationToken);
-            //await _offeringsAgent.InvokeAgentAsync(turnContext.Activity.Text, turnContext, chatHistory, cancellationToken);
-
-            // Invoke the WeatherForecastAgent to process the message
-            //await foreach (StreamingChatMessageContent response in _offeringsAgent.InvokeAgentAsync(turnContext.Activity.Text, chatHistory))
-            //{
-            //    if (response == null)
-            //    {
-            //        turnContext.StreamingResponse.QueueTextChunk("Sorry, I couldn't get the offerings at the moment.");
-            //        await turnContext.StreamingResponse.EndStreamAsync(cancellationToken);
-            //        return;
-            //    }
-
-            //    chatHistory.Add(response);
-            //    turnContext.StreamingResponse.QueueTextChunk(response.Content);
-            //}
-
-            ////turnContext.StreamingResponse.QueueTextChunk(GetVideoAdaptiveCard());
-            //turnContext.StreamingResponse.FinalMessage = MessageFactory.Attachment(new Attachment()
-            //{
-            //    ContentType = "application/vnd.microsoft.card.adaptive",
-            //    Content = GetVideoAdaptiveCard(),
-            //});
-
-            //await turnContext.StreamingResponse.EndStreamAsync(cancellationToken); // End the streaming response
+            await orchestratorAgent.InvokeAgentAsync(message, chatHistory, cancellationToken);
         }
 
         private async Task WelcomeMessageAsync(ITurnContext turnContext, ITurnState turnState, CancellationToken cancellationToken)
@@ -93,48 +57,9 @@ namespace ElAgentApi.Bot
             {
                 if (member.Id != turnContext.Activity.Recipient.Id)
                 {
-                    await turnContext.SendActivityAsync(MessageFactory.Text("Hello and Welcome! I'm here to help with all your offerings and program needs!"), cancellationToken);
+                    await turnContext.SendActivityAsync(MessageFactory.Text("Hello and Welcome! I'm here to help with all your questions!"), cancellationToken);
                 }
             }
-        }
-
-        private string GetVideoAdaptiveCard()
-        {
-            // This method can be used to return an adaptive card for video content.
-            // For now, it returns a placeholder string.
-            return """
-            {
-              "type": "AdaptiveCard",
-              "version": "1.5",
-              "body": [
-                {
-                  "type": "Media",
-                   "altText": "This is a test video",
-                    "poster": "https://adaptivecards.microsoft.com/images/adaptivecards1.jpeg",
-                    "id": "media",
-                    "sources": [
-                        {
-                            "mimeType": "video/mp4",
-                            "url": "https://employeehub-my.sharepoint.com/:v:/g/personal/prbona_employeehub_onmicrosoft_com/ETG4mZkmvspKg2ap8YcAl10BnTC9Gc7zQBx0CD76GGj0Ig?e=21bggE&nav=eyJyZWZlcnJhbEluZm8iOnsicmVmZXJyYWxBcHAiOiJTdHJlYW1XZWJBcHAiLCJyZWZlcnJhbFZpZXciOiJTaGFyZURpYWxvZy1MaW5rIiwicmVmZXJyYWxBcHBQbGF0Zm9ybSI6IldlYiIsInJlZmVycmFsTW9kZSI6InZpZXcifX0%3D"
-                        }
-                    ]
-                },
-                {
-                  "type": "TextBlock",
-                  "text": "Telegraph Road - Dire Straits",
-                  "size": "Large",
-                  "weight": "Bolder",
-                  "wrap": true
-                },
-                {
-                  "type": "TextBlock",
-                  "spacing": "None",
-                  "text": "Cover by David Claux",
-                  "wrap": true
-                }
-              ]
-            }
-            """;
         }
     }
 }
