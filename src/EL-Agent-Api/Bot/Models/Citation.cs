ca904bbae89adde847427f5124c4ad158c6eb940
Praveen Kumar Bonakurthi <prbona@microsoft.com>
2025-07-15 17:41:42 -0700

    Clean up file

--- src/EL-Agent-Api/Bot/Models/Citation.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace ElAgentApi.Bot.Models
-{
-    public class Citation(string id, string title, string url)
-    {
-        public string Id { get; set; } = id;
-        public string Title { get; set; } = title;
-        public string Url { get; set; } = url;
-    }   
-}
